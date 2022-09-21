f0f18d5535e6d7e66ecd7569eb44f2b9750f4323
Niguel Price <niguelprice@yahoo.com>
2022-09-21 12:41:53 -0400

    updated files and projects

--- Product/views.py
+++ Product/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from itertools import product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+
+
+@api_view(['GET'])
+def Products_list(request):
+    product = Product.objects.all()
+
+    serializer = ProductSerializer(product, many=True)
+
+    return Response(serializer.data)
--- Product_project/settings.py
+++ Product_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from pickle import FALSE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,6 +68,10 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False
+}
 
 WSGI_APPLICATION = 'Product_project.wsgi.application'
 
